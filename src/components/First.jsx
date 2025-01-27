353f87d1543cb95a3b548234b81be889fc84e9b0
Tushka555 <tuvshee894@gmail.com>
2025-01-27 22:36:58 +0800

    m

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -1,3 +1,4 @@
+import {Moon} from "./Moon"
 export const First = () => {
   return (
     <>
@@ -11,7 +12,7 @@ export const First = () => {
             <div> helo</div>
             <div> helo </div>
           </div>
-          <button class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 w-9"></button>
+          <button class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 w-9"><Moon/></button>
         </div>
       </div>
     </>
